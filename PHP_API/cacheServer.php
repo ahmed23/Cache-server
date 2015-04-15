1fb1fba3c471e4d4dbe82a5bb85439253c70f5b9
antlil <liljeberg.anton@gmail.com>
2015-04-15 13:18:14 +0200

    php api shell

---
+++ PHP_API/cacheServer.php
@@ -0,0 +1,32 @@
+<?php
+
+class cacheServer{
+
+	public function connect($ip, int $port){
+
+	}
+
+	public function disconnect(){
+
+	}
+
+	public function get($key){
+
+	}
+
+	public function remove($key){
+
+	}
+
+	public function removeAsync($key){
+		
+	}
+
+	public function add($object, $key){
+
+	}
+
+}
+
+
+?>
